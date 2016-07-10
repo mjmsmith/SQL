f1c35e898c259f21d4b3bb872055ab268f7c745d
Mark Smith <mark@camazotz.com>
2016-07-10 14:05:08 -0400

    Use C7 instead of URI.

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "SQL",
     dependencies: [
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/Zewo/URI.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 9),
     ]
 )
