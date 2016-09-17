2044670883a4919d585253716462de85b50a3bf3
Mark Smith <mark@camazotz.com>
2016-09-17 17:06:01 -0400

    Don't need Strings package.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SQL",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 12),
-        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 12)
     ]
 )
