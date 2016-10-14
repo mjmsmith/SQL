6931d06006feac426537eae45bc277ba89a675b1
Mark Smith <mark@camazotz.com>
2016-10-13 21:38:55 -0400

    Remove C7 dependency.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,5 @@ import PackageDescription
 let package = Package(
     name: "SQL",
     dependencies: [
-        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 12)
     ]
 )
